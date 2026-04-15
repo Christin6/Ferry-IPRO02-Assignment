4e2fd948d07b7b202f09ac6f7b423345282a3ae9
Glennoy <259489953+papijoni@users.noreply.github.com>
2026-04-15 17:37:39 +1000

    a

--- FerryManagement.java
+++ FerryManagement.java
@@ -6,7 +6,6 @@ public class FerryManagement {
     private HashMap<Ferry, ArrayList<FerryTrip>> trips;
     private HashMap<Customer, ArrayList<FerryTrip>> customerBookings;
 
-
     FerryManagement() {
         trips = new HashMap<>();
         customerBookings = new HashMap<>();
@@ -17,32 +16,28 @@ public class FerryManagement {
         trips.get(ferry).add(trip);
     }
 
-    void bookTrip(Customer customer, FerryTrip trip){
-        for (ArrayList<FerryTrip> tripList : trips.values()){
-            if (tripList.contains(trip)){
+    void bookTrip(Customer customer, FerryTrip trip) {
+        for (ArrayList<FerryTrip> tripList : trips.values()) {
+            if (tripList.contains(trip)) {
                 customerBookings.put(customer, new ArrayList<>());
                 customerBookings.get(customer).add(trip);
             }
         }
     }
 
-    void checkBooking(Customer customer){
+    void checkBooking(Customer customer) {
         ArrayList<FerryTrip> bookingList = customerBookings.get(customer);
-        if(bookingList.isEmpty()){
-            System.out.println("No booking made.");
-        } else {
-            for (FerryTrip trip : bookingList){
-                System.out.println(trip);
-            }
+        for (FerryTrip trip : bookingList) {
+            System.out.println(trip);
         }
     }
-    
 
     ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String destination, String startingPoint) {
         return new ArrayList<>();
     }
 
-    ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String destination, String startingPoint, double priceMaximum) {
+    ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String destination, String startingPoint,
+            double priceMaximum) {
         return new ArrayList<>();
     }
 
@@ -52,7 +47,7 @@ public class FerryManagement {
 }
 
 interface AssignDiscount {
-  void assignDiscount(double amount, FerryTrip tripTarget);
+    void assignDiscount(double amount, FerryTrip tripTarget);
 
-  void assignDiscount(int percentage, FerryTrip tripTarget);
+    void assignDiscount(int percentage, FerryTrip tripTarget);
 }
