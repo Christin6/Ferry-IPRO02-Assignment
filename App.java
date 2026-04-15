fa81b71240ac0214625959b21091f2ebb788551d
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-04-15 15:51:42 +1000

    add dummy code

--- App.java
+++ App.java
@@ -1,5 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.Month;
+
 public class App {
     public static void main(String[] args) {
+        FerryManagement ferryManagement = new FerryManagement();
+
+        Ferry f1 = new Ferry("F1", 100);
+        Ferry f2 = new Ferry("F2", 100);
+        Ferry f3 = new Ferry("F3", 100);
+
+        FerryTrip t1 = new FerryTrip("Sydney", "Jakarta", 80, LocalDateTime.of(2026, Month.MAY, 15, 10, 30));
+        FerryTrip t2 = new FerryTrip("Jakarta", "Sydney", 70, LocalDateTime.of(2026, Month.MAY, 20, 12, 30));
+        FerryTrip t3 = new FerryTrip("Kuala Lumpur", "Tokyo", 120, LocalDateTime.of(2026, Month.DECEMBER, 13, 9, 30));
+        FerryTrip t4 = new FerryTrip("Tokyo", "Kuala Lumpur", 130, LocalDateTime.of(2026, Month.DECEMBER, 16, 9, 30));
+        FerryTrip t5 = new FerryTrip("London", "Paris", 180, LocalDateTime.of(2026, Month.NOVEMBER, 28, 16, 30));
+        FerryTrip t6 = new FerryTrip("Paris", "London", 190, LocalDateTime.of(2026, Month.DECEMBER, 2, 14, 30));
+
+        ferryManagement.addFerryTrip(f1, t1);
+        ferryManagement.addFerryTrip(f1, t2);
+        ferryManagement.addFerryTrip(f2, t3);
+        ferryManagement.addFerryTrip(f2, t4);
+        ferryManagement.addFerryTrip(f3, t5);
+        ferryManagement.addFerryTrip(f3, t6);
+
         App app = new App();
         app.runMainMenu();
     }
@@ -41,20 +64,15 @@ public class App {
             if (choice == 0) {
                 System.out.println("Returning to main menu...");
                 break;
-            }
-            else if (choice == 1) {
-                
-            }
-            else if (choice == 2) {
+            } else if (choice == 1) {
 
-            }
-            else if (choice == 3) {
+            } else if (choice == 2) {
 
-            }
-            else if (choice == 4) {
-                
-            }
-            else {
+            } else if (choice == 3) {
+
+            } else if (choice == 4) {
+
+            } else {
                 System.out.println("Please choose 0, 1, 2, 3, or 4,");
             }
         }
@@ -72,20 +90,15 @@ public class App {
         while (true) {
             if (choice == 0) {
                 break;
-            }
-            else if (choice == 1) {
+            } else if (choice == 1) {
 
-            }
-            else if (choice == 2) {
+            } else if (choice == 2) {
 
-            }
-            else if (choice == 3) {
+            } else if (choice == 3) {
 
-            }
-            else if (choice == 4) {
-                
-            }
-            else {
+            } else if (choice == 4) {
+
+            } else {
                 System.out.println("Please choose 0, 1, 2, 3, or 4,");
             }
         }
--- FerryManagement.java
+++ FerryManagement.java
@@ -14,11 +14,11 @@ public class FerryManagement {
         trips.get(ferry).add(trip);
     }
 
-    ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String detination, String startingPoint) {
+    ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String destination, String startingPoint) {
       return new ArrayList<>();
     }
 
-    ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String detination, String startingPoint, double priceMaximum) {
+    ArrayList<FerryTrip> getAvailability(LocalDateTime dateTime, String destination, String startingPoint, double priceMaximum) {
       return new ArrayList<>();
     }
 
@@ -31,38 +31,4 @@ interface AssignDiscount {
   void assignDiscount(double amount, FerryTrip tripTarget);
 
   void assignDiscount(int percentage, FerryTrip tripTarget);
-}
-
-public class Admin {
-  private String name;
-  private ArrayList<Ferry> managedFerries;
-  private ArrayList<FerryTrip> managedTrips;
-
-  Admin(String name) {
-    this.name = name;
-    this.managedTrips = new ArrayList<>();
-    this.managedFerries = new ArrayList<>();
-  }
-
-  Ferry f1 = new Ferry("F1", 100);
-  Ferry f2 = new Ferry("F2", 100);
-  Ferry f3 = new Ferry("F3", 100);
-
-  void addFerry() {
-    this.managedFerries.add(f1);
-    this.managedFerries.add(f2);
-    this.managedFerries.add(f3);
-  }
-
-  void assignFerryTrip(FerryTrip trip, Ferry ferry) {
-
-  }
-
-  void getAllTrips() {
-
-  }
-
-  public String toString(){
-    return 
-  }
 }
