f03fd7c4a1a6472b879fe587ee975f171d3c64be
Glennoy <259489953+papijoni@users.noreply.github.com>
2026-04-29 15:54:12 +1000

    COMMEH

--- mvcGUIApp/FerryApp.java
+++ mvcGUIApp/FerryApp.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -7,8 +9,15 @@ import javafx.stage.Stage;
 public class FerryApp extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Ferry Management App");
-        Label label = new Label("Text");
-        VBox root = new VBox(label);
+        Label loginLabel = new Label("LOGIN PAGE");
+
+        Button customerBtn = new Button("Customer");
+
+        Button adminBtn = new Button("Admin");
+
+        VBox root = new VBox(loginLabel, customerBtn, adminBtn);
+        root.setAlignment(Pos.CENTER);
+
         Scene scene = new Scene(root, 600, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
