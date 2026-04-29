4ed2d3b4801a897b8d0e1ffe7ab5d142f8e98c59
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-04-29 12:01:28 +1000

    create basic ui

---
+++ mvcGUIApp/FerryApp.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class FerryApp extends Application {
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Ferry Management App");
+        Label label = new Label("Text");
+        VBox root = new VBox(label);
+        Scene scene = new Scene(root, 600, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
