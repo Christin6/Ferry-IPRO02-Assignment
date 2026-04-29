f5ead01ea1a453fc9d974a183b43fe670cca2181
Christin6 <94294443+Christin6@users.noreply.github.com>
2026-04-29 15:42:26 +1000

    real fix for AppModel.java

--- mvcGUIApp/AppModel.java
+++ mvcGUIApp/AppModel.java
@@ -1,21 +1,23 @@
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
 public class AppModel {
-    private final ObservableList<Ferry> ferries;
+    private final ObservableList<FerryTrip> trips;
 
     AppModel() {
-        this.ferryManagement = new FerryManagement();
         // dummy data
         Ferry f1 = new Ferry("F1", 100);
         Ferry f2 = new Ferry("F2", 100);
         Ferry f3 = new Ferry("F3", 100);
         Ferry f4 = new Ferry("F4", 100);
 
-        FerryTrip t1 = new FerryTrip("Sydney", "Jakarta", 80, f1);
-        FerryTrip t2 = new FerryTrip("Jakarta", "Sydney", 70, f1);
-        FerryTrip t3 = new FerryTrip("Kuala Lumpur", "Tokyo", 120, f2);
-        FerryTrip t4 = new FerryTrip("Tokyo", "Kuala Lumpur", 130, f3);
-        FerryTrip t5 = new FerryTrip("London", "Paris", 180, f4);
-        FerryTrip t6 = new FerryTrip("Paris", "London", 190, f4);
-
-
+        this.trips = FXCollections.observableArrayList(
+            new FerryTrip("Sydney", "Jakarta", 80, f1),
+            new FerryTrip("Jakarta", "Sydney", 70, f1),
+            new FerryTrip("Kuala Lumpur", "Tokyo", 120, f2),
+            new FerryTrip("Tokyo", "Kuala Lumpur", 130, f3),
+            new FerryTrip("London", "Paris", 180, f4),
+            new FerryTrip("Paris", "London", 190, f4)
+        );
     }
 }
